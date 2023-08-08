d08838f559f5189f8fedbcb674b922332a3d80ae
Kola Kolade <kolakolade@gmail.com>
2023-08-08 17:46:18 +0100

    Update Inventory model and view to remove user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_07_164143) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_163655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
@@ -37,5 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_07_164143) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "inventories", "users"
   add_foreign_key "recipes", "users"
 end

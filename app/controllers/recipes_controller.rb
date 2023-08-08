a4d9298ba54181d01716e3bcda5dc16dbfbcc89b
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-08 17:22:15 -0300

    added add food form, and foods and recipe_foods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_recipe, only: [:show, :edit, :update, :destroy, :toggle_public]
+  before_action :set_recipe, only: %i[show edit update destroy toggle_public]
 
   def index
     @recipes = current_user.recipes.all
@@ -12,28 +12,25 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = current_user.recipes.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods
   end
 
   def toggle_public
     @recipe.update(public: !@recipe.public)
     redirect_to @recipe, notice: 'Recipe public status toggled.'
   end
-  
 
   def destroy
-    begin
-      @recipe = current_user.recipes.find(params[:id])
-      @recipe.destroy
-      redirect_to recipes_path, notice: 'Recipe was successfully deleted.'
-    rescue ActiveRecord::RecordNotFound
-      redirect_to recipes_path, alert: 'Recipe not found.'
-    rescue => e
-      redirect_to recipes_path, alert: "Error: #{e.message}"
-    end
+    @recipe = current_user.recipes.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, notice: 'Recipe was successfully deleted.'
+  rescue ActiveRecord::RecordNotFound
+    redirect_to recipes_path, alert: 'Recipe not found.'
+  rescue StandardError => e
+    redirect_to recipes_path, alert: "Error: #{e.message}"
   end
 
-  def modal
-  end
+  def modal; end
 
   private
 

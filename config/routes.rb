40d1e9a6dfaecfe5245830d28230f120473ef28d
Kola Kolade <kolakolade@gmail.com>
2023-08-08 17:39:15 +0100

    Generate resources for Inventories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :inventories
   devise_for :users, path: '', path_names: { sign_in: 'login' }
   root "recipes#public_index"
   resources :recipes, only: [:index]

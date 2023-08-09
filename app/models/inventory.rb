ba909824682c9a67c1efa1deab506d1b345e8430
Kola Kolade <kolakolade@gmail.com>
2023-08-09 23:27:41 +0100

    Fix linter errors

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@ class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods
   has_many :foods, through: :inventory_foods
-  
+
   attribute :description, :text
 
   validates :name, presence: true
--- spec/models/inventories_spec.rb
+++ spec/models/inventories_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Inventories, type: :model do
-# Create a new inventory
-inventory = Inventory.new(name: "My Inventory")
+  # Create a new inventory
+  inventory = Inventory.new(name: 'My Inventory')
 
-# Associate the inventory with a user
-user = User.find_by(id: 1)
-inventory.user = user
+  # Associate the inventory with a user
+  user = User.find_by(id: 1)
+  inventory.user = user
 
-# Validate and save the inventory
-if inventory.valid?
-  inventory.save
-else
-  puts "Inventory is invalid: #{inventory.errors.full_messages.join(', ')}"
-end
-
+  # Validate and save the inventory
+  if inventory.valid?
+    inventory.save
+  else
+    puts "Inventory is invalid: #{inventory.errors.full_messages.join(', ')}"
+  end
 end

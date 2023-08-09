f1387e336af2cb2a38fc9730c3c7dc294a703626
jlvFlores <jluisvazquez.jv@gmail.com>
2023-08-09 12:55:53 -0600

    Update new foods form

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -4,16 +4,14 @@ class FoodsController < ApplicationController
   end
   
   def new
-    @recipe = Recipe.includes(:recipe_foods).find(params[:recipe_id])
     @food = Food.new
   end
 
   def create
-    @recipe = Recipe.includes(:recipe_foods).find(params[:recipe_id])
-    @food = @recipe.foods.build(food_params)
+    @food = Food.new(food_params)
 
     if @food.save
-      redirect_to recipe_path(@recipe), notice: 'Food was successfully added.'
+      redirect_to foods_path, notice: 'Food was successfully added.'
     else
       render :new
     end

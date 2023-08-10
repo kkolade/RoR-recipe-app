1986b6b5f9d6d5c5c3b75664083e4b2509390928
Kola Kolade <kolakolade@gmail.com>
2023-08-10 09:18:13 +0100

    Refactor test for inventory model

--- spec/models/inventories_spec.rb
+++ spec/models/inventories_spec.rb
@@ -1,17 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe Inventories, type: :model do
-  # Create a new inventory
-  inventory = Inventory.new(name: 'My Inventory')
+RSpec.describe Inventory, type: :model do
+  let(:user) { create(:user) } # Assuming you have a User factory
 
-  # Associate the inventory with a user
-  user = User.find_by(id: 1)
-  inventory.user = user
+  it "is valid with valid attributes" do
+    inventory = Inventory.new(name: "Test Inventory", user: user)
+    expect(inventory).to be_valid
+  end
 
-  # Validate and save the inventory
-  if inventory.valid?
-    inventory.save
-  else
-    puts "Inventory is invalid: #{inventory.errors.full_messages.join(', ')}"
+  it "is not valid without a name" do
+    inventory = Inventory.new(user: user)
+    expect(inventory).to_not be_valid
+    expect(inventory.errors[:name]).to include("can't be blank")
+  end
+
+  it "belongs to a user" do
+    association = Inventory.reflect_on_association(:user)
+    expect(association.macro).to eq(:belongs_to)
+  end
+
+  it "has many inventory_foods" do
+    association = Inventory.reflect_on_association(:inventory_foods)
+    expect(association.macro).to eq(:has_many)
+  end
+
+  it "has many foods through inventory_foods" do
+    association = Inventory.reflect_on_association(:foods)
+    expect(association.macro).to eq(:has_many)
+    expect(association.options[:through]).to eq(:inventory_foods)
+  end
+
+  it "has an attribute description of type text" do
+    inventory = Inventory.new(name: "Test Inventory", user: user)
+    expect(inventory.attributes).to include("description")
+    expect(inventory.attribute_types["description"].type).to eq(:text)
   end
 end

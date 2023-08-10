f450af649dc0877456c2492c4a1a63e0c3570428
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-08-10 19:05:21 -0300

    fixed linters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'database_cleaner'
-require 'factory_bot_rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
